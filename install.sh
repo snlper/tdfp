a289503738e4c980a83aad3840ea3cf86ef01a8e
snlper <kotlybulatowadim@gmail.com>
2024-08-13 01:00:32 +0300

    add url for ffmpeg

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ exec &> >(tee -a "$INSTALL_LOG")
 
 #apt install libcurl4-gnutils-dev
 
+# загрузка всей хуйни
+
+# загрузка transmission-cli
 
 BEGIN_TORRENT_URL="https://build.transmissionbt.com/job/trunk-linux/"
 END_TORRENT_URL=$(curl -v --silent https://build.transmissionbt.com/job/trunk-linux/ 2>&1 | grep -Eo "lastSuccessfulBuild/artifact/transmission[a-zA-Z0-9./?=_%:+-]*.tar.xz" | uniq)
@@ -13,6 +16,13 @@ TORRENT_ARCHIVE_FILE=$(echo $END_TORRENT_URL | sed 's/.*\///')
 TORRENT_DIR=$(echo $TORRENT_ARCHIVE_FILE | sed -r 's/.tar.xz//')
 
 wget $BEGIN_TORRENT_URL$END_TORRENT_URL
+
+# загрузка ffmpeg
+
+FFMPEG_URL=$(curl -v --silent https://www.ffmpeg.org/download.html 2>&1 | grep -m1 "https://ffmpeg.org/releases/ffmpeg-" | sed -r 's/(.+tar.xz).+/\1/' | sed 's/.*h/h/')
+echo $FFMPEG_URL
+
+exit 0
 
 tar xf $TORRENT_ARCHIVE_FILE
 
