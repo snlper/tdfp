89b0c2095ab1d168706753681844740b93938169
snlper <kotlybulatowadim@gmail.com>
2024-08-14 17:17:23 +0300

    добавил проверку и разукрасил сборку

--- install.sh
+++ install.sh
@@ -4,13 +4,31 @@ INSTALL_LOG="tdfp_install.log"
 exec &> >(tee -a "$INSTALL_LOG")
 
 # проверка наличия необходимого ПО
-if [ ! -x /usr/bin/wget ]; then
-	echo "Install wget"
-	exit 0
+if [ ! -x /usr/bin/curl ]; then
+	echo "Install curl"
+	touch /dev/null
+	exit 1
 fi
 
-#apt install libcurl4-gnutils-dev
+dpkg-query -s libcurl4-openssl-dev &>>$INSTALL_LOG
+if [[ $? == 1 ]]; then
+	echo "Need to install libcurl4-openssl-dev"
+	read -p "Install? (yes/no) " yn
+	case $yn in
+		yes )	echo "ok, let me do it";
+			sudo apt install -y libcurl4-openssl-dev;;
+		no )	echo "exiting...";
+			exit 3;;
+		* )	echo "invalid response";
+			exit 4;;
+	esac
+fi
+
+# frei0r-plugins-dev
+# libchromaprint-dev
+
 WORKING_FOLDER=$PWD
+JOBS_COUNT=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 # загрузка всей хуйни
 
@@ -35,10 +53,11 @@ FFMPEG_ARCHIVE_FILE=$(echo $FFMPEG_URL | sed 's/.*\///')
 FFMPEG_DIR=$(echo $FFMPEG_ARCHIVE_FILE | sed -r 's/.tar.xz//')
 
 # загрузка исходников transmission и ffmpeg
+echo -ne 'Downloading sources\n'
 echo -ne '[    ]   0%\r'
-wget -q $TORRENT_URL
+curl -O --silent $TORRENT_URL
 echo -ne '[#   ]  25%\r'
-wget -q $FFMPEG_URL
+curl -O --silent $FFMPEG_URL
 echo -ne '[##  ]  50%\r'
 
 # распаковка исходников
@@ -46,23 +65,31 @@ tar xf $TORRENT_ARCHIVE_FILE
 echo -ne '[### ]  75%\r'
 tar xf $FFMPEG_ARCHIVE_FILE
 echo -ne '[####] 100%\n'
-echo -ne 'Done\n'
+echo -ne 'Downloading done\n'
 
-# очистка от мусора
+# очистка от скачанных архивов
 rm $TORRENT_ARCHIVE_FILE $FFMPEG_ARCHIVE_FILE
 
 # сборка
 # сборка transmission
 cd $TORRENT_DIR
-cmake -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo
+echo -ne 'Building\n'
+echo -ne '[    ]   0%\r'
+cmake -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo &>>$INSTALL_LOG
 cd build
-cmake --build .
-
+echo -ne '[#   ]  25%\r'
+cmake --build . --parallel $JOBS_COUNT &>>$INSTALL_LOG
+echo -ne '[##  ]  50%\r'
 cd $WORKING_FOLDER
 
 # сборка ffmpeg
 cd $FFMPEG_DIR
-bash configure --arch=amd64 --enable-gpl --disable-stripping --enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libdav1d --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libglslang --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg --enable-libopenmpt --enable-libopus --enable-libpulse --enable-librabbitmq --enable-librist --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libsrt --enable-libssh --enable-libsvtav1 --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzimg --enable-libzmq --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opencl --enable-opengl --enable-sdl2 --disable-sndio --enable-libjxl --enable-pocketsphinx --enable-librsvg --enable-libmfx --enable-libdc1394 --enable-libdrm --enable-libiec61883 --enable-chromaprint --enable-frei0r --enable-libx264 --enable-libplacebo --enable-librav1e --enable-shared
-make -j4
+bash configure --arch=amd64 --enable-gpl --disable-stripping --enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libdav1d --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libglslang --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg --enable-libopenmpt --enable-libopus --enable-libpulse --enable-librabbitmq --enable-librist --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libsrt --enable-libssh --enable-libsvtav1 --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzimg --enable-libzmq --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opencl --enable-opengl --enable-sdl2 --disable-sndio --enable-libjxl --enable-pocketsphinx --enable-librsvg --enable-libmfx --enable-libdc1394 --enable-libdrm --enable-libiec61883 --enable-chromaprint --enable-frei0r --enable-libx264 --enable-libplacebo --enable-librav1e --enable-shared &>>$INSTALL_LOG
+echo -ne '[### ]  75%\r'
+make -j $JOBS_COUNT &>>$INSTALL_LOG
+echo -ne '[####]  100%\n'
+echo -ne 'Building done\n'
 
 cd $WORKING_FOLDER
+ls
+
